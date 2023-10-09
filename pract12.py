d20f7e60e2ba4298b9c030479dbbc9906e2744fd
manighahrmani <mashapicasso@gmail.com>
2023-10-09 21:42:20 +0100

    Add perliminary examples

--- pract12.py
+++ pract12.py
@@ -1,42 +1,38 @@
-import tkinter as tk
-from tkinter import ttk
+from tkinter import *
 
 
 class LoginApp:
-    def __init__(self, master):
-        self.master = master
-        self.master.title("Employee Login")
+    def __init__(self, win):
+        self.win = win
+        self.win.title("Employee Login")
 
-        self.mainframe = ttk.Frame(self.master)
-        self.mainframe.grid(column=0, row=0,)
+        self.mainframe = Frame(self.win)
+        self.mainframe.grid(column=0, row=0)
 
-        self.username = tk.StringVar()
-        self.password = tk.StringVar()
-        self.message = tk.StringVar()
+        self.username = StringVar()
+        self.password = StringVar()
+        self.message = StringVar()
         self.message.set(
             "Please enter your username and password then sign in")
 
         self.create_widgets()
 
     def create_widgets(self):
-        ttk.Label(self.mainframe, textvariable=self.message).grid(
+        Label(self.mainframe, textvariable=self.message).grid(
             column=1, row=0, columnspan=2)
 
-        ttk.Label(self.mainframe, text="Username:").grid(column=1, row=1)
-        ttk.Entry(self.mainframe, width=20,
-                  textvariable=self.username).grid(column=2, row=1)
+        Label(self.mainframe, text="Username:").grid(column=1, row=1)
+        Entry(self.mainframe, width=20,
+              textvariable=self.username).grid(column=2, row=1)
 
-        ttk.Label(self.mainframe, text="Password:").grid(column=1, row=2)
-        ttk.Entry(self.mainframe, width=20, textvariable=self.password,
-                  show='*').grid(column=2, row=2)
+        Label(self.mainframe, text="Password:").grid(column=1, row=2)
+        Entry(self.mainframe, width=20, textvariable=self.password,
+              show='*').grid(column=2, row=2)
 
-        ttk.Button(self.mainframe, text="Sign In",
-                   command=self.authenticate).grid(column=1, row=3)
-        ttk.Button(self.mainframe, text="Cancel",
-                   command=self.master.quit).grid(column=2, row=3)
-
-        for child in self.mainframe.winfo_children():
-            child.grid_configure(padx=5, pady=5)
+        Button(self.mainframe, text="Sign In",
+               command=self.authenticate).grid(column=1, row=3)
+        Button(self.mainframe, text="Cancel",
+               command=self.win.quit).grid(column=2, row=3)
 
     def authenticate(self):
         username = self.username.get()
@@ -57,7 +53,7 @@ class LoginApp:
             self.message.set("Login successful!")
 
 
-if __name__ == '__main__':
-    root = tk.Tk()
+def main():
+    root = Tk()
     app = LoginApp(root)
     root.mainloop()
