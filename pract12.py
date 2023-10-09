12debdfadd3f8e4028f5374ab473588250c2b59f
manighahrmani <mashapicasso@gmail.com>
2023-10-09 22:21:47 +0100

    Update pract12.py

--- pract12.py
+++ pract12.py
@@ -2,9 +2,10 @@ from tkinter import *
 
 
 class LoginApp:
-    def __init__(self, win):
-        self.win = win
+    def __init__(self):
+        self.win = Tk()
         self.win.title("Employee Login")
+        self.win.geometry("250x100")
 
         self.mainframe = Frame(self.win)
         self.mainframe.grid(column=0, row=0)
@@ -12,24 +13,25 @@ class LoginApp:
         self.username = StringVar()
         self.password = StringVar()
         self.message = StringVar()
-        self.message.set(
-            "Please enter your username and password then sign in")
+        self.message.set("Enter username and password.")
 
         self.create_widgets()
 
+        self.win.mainloop()
+
     def create_widgets(self):
-        Label(self.mainframe, textvariable=self.message).grid(
+        Label(self.mainframe, textvariable=self.message, width=30).grid(
             column=1, row=0, columnspan=2)
 
         Label(self.mainframe, text="Username:").grid(column=1, row=1)
-        Entry(self.mainframe, width=20,
+        Entry(self.mainframe, width=25,
               textvariable=self.username).grid(column=2, row=1)
 
         Label(self.mainframe, text="Password:").grid(column=1, row=2)
-        Entry(self.mainframe, width=20, textvariable=self.password,
+        Entry(self.mainframe, width=25, textvariable=self.password,
               show='*').grid(column=2, row=2)
 
-        Button(self.mainframe, text="Sign In",
+        Button(self.mainframe, text="Sign in",
                command=self.authenticate).grid(column=1, row=3)
         Button(self.mainframe, text="Cancel",
                command=self.win.quit).grid(column=2, row=3)
@@ -46,14 +48,11 @@ class LoginApp:
         }
 
         if username not in loginDetails:
-            self.message.set("Username not found")
+            self.message.set("Username not found.")
         elif loginDetails[username] != password:
-            self.message.set("Incorrect password")
+            self.message.set("Incorrect password.")
         else:
             self.message.set("Login successful!")
 
 
-def main():
-    root = Tk()
-    app = LoginApp(root)
-    root.mainloop()
+app = LoginApp()
